0c393759c9c52b7e0a172a29d1acf5185ff81997
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-06-13 00:04:07 +0100

    Юля Тімашенка стала готкою
    implementation the black-white change

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib.image as imread
+import os
+from sklearn.preprocessing import Binarizer
+from PIL import Image
 
 def checker(eigenvalues, eigenvectors, matrix):
     for i in range(len(eigenvalues)):
         if not np.allclose(np.dot(matrix, eigenvectors[:, i]), eigenvalues[i] * eigenvectors[:, i]):
             return False
     return True
+
+
+def display_image(image_array):
+    plt.imshow(image_array, cmap='gray')
+    plt.show()
 
 
 def main():
@@ -16,9 +25,19 @@ def main():
         print("Something went wrong")
         return
 
-    print("Everything is correct")
-    print("Eigenvalues: ", eigenvalues)
-    print("Eigenvectors: ", eigenvectors)
+    image_raw = imread.imread("image.jpg")
+
+    image_sum = image_raw.sum(axis=2)
+    print(image_sum.shape)
+    image_bw = image_sum / image_sum.max()
+    print(image_bw.max())
+
+    display_image(image_raw)
+    display_image(image_bw)
+
+    img_vector = image_raw.shape
+
+
 
 
 if __name__ == "__main__":
