d7b62961145efc35d8d22654e26c9f652c863772
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-06-13 00:17:28 +0100

    some changes and task 2 is ready

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from sklearn.preprocessing import Binarizer
 from PIL import Image
 
+
 def checker(eigenvalues, eigenvectors, matrix):
     for i in range(len(eigenvalues)):
         if not np.allclose(np.dot(matrix, eigenvectors[:, i]), eigenvalues[i] * eigenvectors[:, i]):
@@ -25,19 +26,33 @@ def main():
         print("Something went wrong")
         return
 
+    # read image
     image_raw = imread.imread("image.jpg")
 
-    image_sum = image_raw.sum(axis=2)
-    print(image_sum.shape)
-    image_bw = image_sum / image_sum.max()
-    print(image_bw.max())
+    # print the size of the original image and the number of color channels
+    print("Original image")
+    print("Image size (height, width):", image_raw.shape[:2])
+    print("Number of color channels:", image_raw.shape[2], "\n")
 
+    # convert to grayscale
+    image_sum = image_raw.sum(axis=2)
+    image_bw = image_sum / image_sum.max()
+
+    # better way to convert to grayscale
+    image_gray = np.dot(image_raw[...,:3], [0.2989, 0.5870, 0.1140])
+    image_bw2 = image_gray / image_gray.max()
+
+    # print the size of the bw image and the number of color channels
+    print("Grayscale image")
+    print("Image size (height, width):", image_bw2.shape[:2])
+    print("Number of color channels: 1 (cause it is black-shaded, lol)\n")
+
+    # display image
     display_image(image_raw)
     display_image(image_bw)
+    display_image(image_bw2)
 
     img_vector = image_raw.shape
-
-
 
 
 if __name__ == "__main__":
