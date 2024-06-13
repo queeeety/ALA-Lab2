b4efa7dfdb181c53a2837b22b5164c4a9e4be43e
Tymofii Bezverkhyi <148719219+queeeety@users.noreply.github.com>
2024-06-14 00:22:16 +0100

    task 3 is done (cumulative variance and a graph)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as imread
-import os
-from sklearn.preprocessing import Binarizer
+from sklearn.decomposition import PCA
 from PIL import Image
 
 
@@ -35,10 +34,6 @@ def main():
     print("Number of color channels:", image_raw.shape[2], "\n")
 
     # convert to grayscale
-    image_sum = image_raw.sum(axis=2)
-    image_bw = image_sum / image_sum.max()
-
-    # better way to convert to grayscale
     image_gray = np.dot(image_raw[...,:3], [0.2989, 0.5870, 0.1140])
     image_bw2 = image_gray / image_gray.max()
 
@@ -49,10 +44,35 @@ def main():
 
     # display image
     display_image(image_raw)
-    display_image(image_bw)
     display_image(image_bw2)
 
-    img_vector = image_raw.shape
+    ### TASK 3 ###
+
+    # Flatten the image_bw2 matrix into a 2D array
+    image_bw_flattened = image_bw2.reshape(image_bw2.shape[0], -1)
+
+    # Apply PCA to the flattened matrix
+    pca = PCA()
+    pca.fit(image_bw_flattened)
+
+    # Calculate the cumulative variance explained by each principal component
+    cumulative_variance = np.cumsum(pca.explained_variance_ratio_) * 100
+
+    # Знаходження кількості компонент для покриття 95% дисперсії
+    num_components = np.argmax(cumulative_variance >= 95) + 1
+
+    print("Cumulative variance:", len(cumulative_variance))
+    print("Number of components needed to cover 95% of the variance:", num_components)
+
+    # Побудова графіку
+    plt.figure(figsize=(10, 6))
+    plt.plot(cumulative_variance, linewidth=2)
+    plt.axvline(x=num_components, color='k', linestyle='--')
+    plt.axhline(y=95, color='r', linestyle='-')
+    plt.xlabel('Principal components')
+    plt.ylabel('Cumulative Explained Variance')
+    plt.title(f'Number of components explaining 95% variance: {num_components}')
+    plt.show()
 
 
 if __name__ == "__main__":
